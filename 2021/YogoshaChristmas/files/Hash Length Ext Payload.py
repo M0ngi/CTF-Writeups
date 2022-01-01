3f5e5f5050c34e70205258efc2b0f61bd3e8d03b
M0ngi <31113271+M0ngi@users.noreply.github.com>
2022-01-01 01:30:01 +0100

    Add files via upload

---
+++ 2021/YogoshaChristmas/files/Hash Length Ext Payload.py
@@ -0,0 +1,8 @@
+import hlextend
+
+with open('outputPayloads', 'wb') as f
+    for i in xrange(1,50):
+        sha = hlextend.new('sha256')
+        x = sha.extend(":read.php", "guinjutsu.php", i, '184b5d255817fc0afe9316e67c8f386506a3b28b470c94f47583b76c7c0ec1e5')
+        f.write(sha.hexdigest() + '|' + x.replace('\\x', '%')+'\n') # For url encoding
+
